f0075c3413373208d5d1466792696da8147fc77c
ya310 <mentol310@gmail.com>
2019-02-11 09:07:43 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: ['@nuxtjs', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+        semi: false,
+      },
+    ],
+    'no-console': 'off',
+  },
+}
