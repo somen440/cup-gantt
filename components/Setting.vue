87fe203de05765d56a2e33c259f2becd27998060
ya310 <mentol310@gmail.com>
2019-02-11 12:57:18 +0900

    設定を行うコンポーネント追加

---
+++ components/Setting.vue
@@ -0,0 +1,173 @@
+<template>
+  <v-stepper v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step :complete="e1 > 1" step="1">チーム選択</v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step :complete="e1 > 2" step="2">スペース選択</v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step :complete="e1 > 3" step="3">プロジェクト / リスト選択</v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step step="4">確認</v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-select
+          @input="setTeamId"
+          v-validate="'required'"
+          :items="teams"
+          label="teams"
+          data-vv-name="select"
+          required
+          item-text="name"
+          item-value="id"
+        ></v-select>
+
+        <v-btn color="primary" @click="toConfirmTeam" :disabled="!isValidTeamId">Continue</v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-select
+          @input="setSpaceId"
+          v-validate="'required'"
+          :items="spaces"
+          label="spaces"
+          data-vv-name="select"
+          required
+          item-text="name"
+          item-value="id"
+        ></v-select>
+
+        <v-btn color="primary" @click="toConfirmSpace" :disabled="!isValidSpaceId">Continue</v-btn>
+
+        <v-btn flat @click="e1 = 1">Cancel</v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-select
+          @input="setProjectId"
+          v-validate="'required'"
+          :items="projects"
+          label="projects"
+          data-vv-name="select"
+          required
+          item-text="name"
+          item-value="id"
+        ></v-select>
+
+        <v-checkbox v-model="isEnabledList" label="リストを選択する"></v-checkbox>
+
+        <v-select
+          v-if="isEnabledList"
+          @input="setListId"
+          v-validate="'required'"
+          :items="lists"
+          label="lists"
+          data-vv-name="select"
+          required
+          item-text="name"
+          item-value="id"
+        ></v-select>
+
+        <v-btn
+          color="primary"
+          @click="toConfirmSetting"
+          :disabled="(!isValidProjectId || (isEnabledList && !isValidListId))"
+        >Continue</v-btn>
+
+        <v-btn flat @click="e1 = 1">Cancel</v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="4">
+        <v-card v-if="isEnabledSetting">
+          <v-container>
+            <v-flex>
+              <v-list two-line subheader>
+                <v-subheader inset>Settings</v-subheader>
+                <v-list-tile avatar v-for="setting in settings" :key="setting.id">
+                  <v-list-tile-avatar>
+                    <v-icon>build</v-icon>
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ setting.name }}</v-list-tile-title>
+                    <v-list-tile-sub-title>{{ setting.label }}</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+
+                <v-divider inset></v-divider>
+
+                <v-subheader inset>Tasks</v-subheader>
+                <v-list-tile avatar v-for="task in tasks" :key="task.id">
+                  <v-list-tile-avatar>
+                    <v-icon>label</v-icon>
+                  </v-list-tile-avatar>
+                  <v-list-tile-content>
+                    <v-list-tile-title>{{ task.name }}</v-list-tile-title>
+                    <v-list-tile-sub-title>{{ task.label }}</v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </v-list>
+            </v-flex>
+          </v-container>
+        </v-card>
+
+        <v-btn color="primary" @click="finishSetting">Finish</v-btn>
+
+        <v-btn flat @click="e1 = 1">Cancel</v-btn>
+      </v-stepper-content>
+    </v-stepper-items>
+  </v-stepper>
+</template>
+
+<script>
+import { mapActions, mapState, mapMutations, mapGetters } from 'vuex'
+
+export default {
+  data() {
+    return {
+      e1: 0,
+      isEnabledList: false,
+      isEnabledSetting: false,
+    }
+  },
+  computed: {
+    ...mapState(['teams', 'spaces', 'projects', 'tasks']),
+    ...mapGetters([
+      'lists',
+      'isValidTeamId',
+      'isValidSpaceId',
+      'isValidProjectId',
+      'isValidListId',
+      'settings',
+    ]),
+  },
+  created() {
+    this.fetchTeams()
+  },
+  methods: {
+    ...mapActions(['fetchTeams', 'fetchSpaces', 'fetchProjects', 'fetchTasks']),
+    ...mapMutations(['setTeamId', 'setSpaceId', 'setProjectId', 'setListId']),
+    toConfirmTeam() {
+      this.e1 = 2
+      this.fetchSpaces()
+    },
+    toConfirmSpace() {
+      this.e1 = 3
+      this.fetchProjects()
+    },
+    toConfirmSetting() {
+      this.e1 = 4
+      this.fetchTasks().then(() => (this.isEnabledSetting = true))
+    },
+    finishSetting() {
+      confirm('Complete the setup?')
+    },
+  },
+}
+</script>
--- store/index.js
+++ store/index.js
@@ -2,24 +2,37 @@ import moment from 'moment'
 import qs from 'qs'
 
 const Unit = class {
-  constructor(id, name) {
+  constructor(id, name, label) {
     this.id = id
     this.name = name
+    this.label = label
   }
 }
-const Team = class extends Unit {}
-const Space = class extends Unit {}
+const Team = class extends Unit {
+  constructor(id, name) {
+    super(id, name, 'team')
+  }
+}
+const Space = class extends Unit {
+  constructor(id, name) {
+    super(id, name, 'space')
+  }
+}
 const Project = class extends Unit {
   constructor(id, name, lists) {
-    super(id, name)
+    super(id, name, 'project')
     this.lists = lists
   }
 }
-const List = class extends Unit {}
+const List = class extends Unit {
+  constructor(id, name) {
+    super(id, name, 'list')
+  }
+}
 const Assigne = class extends Unit {}
 const Task = class extends Unit {
   constructor(id, name, detail, assigne, startedAt, endedAt) {
-    super(id, name)
+    super(id, name, 'task')
     this.detail = detail
     this.assigne = assigne
     this.startedAt = startedAt
@@ -32,9 +45,51 @@ export const state = () => ({
   spaces: [], // Spaces[],
   projects: [], // Projects[]
   tasks: [], // Task[]
+  teamId: null,
+  spaceId: null,
+  projectId: null,
+  listId: null,
 })
 
-export const getters = {}
+export const getters = {
+  lists: state => {
+    return state.projectId !== null
+      ? state.projects.find(e => e.id === state.projectId).lists
+      : null
+  },
+  isValidTeamId: state => {
+    return state.teamId !== null
+  },
+  isValidSpaceId: state => {
+    return state.spaceId !== null
+  },
+  isValidProjectId: state => {
+    return state.projectId !== null
+  },
+  isValidTasks: state => {
+    return state.tasks.length > 0
+  },
+  isValidSettings: (_, getters) => {
+    return (
+      getters.isValidTeamId &&
+      getters.isValidSpaceId &&
+      getters.isValidProjectId &&
+      getters.isValidTasks
+    )
+  },
+  settings: (state, getters) => {
+    return getters.isValidSettings
+      ? [
+          state.teams.find(e => e.id === state.teamId),
+          state.spaces.find(e => e.id === state.spaceId),
+          state.projects.find(e => e.id === state.projectId),
+          getters.isValidListId
+            ? state.projects.find(e => e.id === state.listId)
+            : new List(-1, '指定なし'),
+        ]
+      : []
+  },
+}
 
 export const mutations = {
   setTeams(state, teams) {
@@ -49,28 +104,32 @@ export const mutations = {
   setTasks(state, tasks) {
     state.tasks = tasks
   },
+  setTeamId(state, teamId) {
+    state.teamId = teamId
+  },
+  setSpaceId(state, spaceId) {
+    state.spaceId = spaceId
+  },
+  setProjectId(state, projectId) {
+    state.projectId = projectId
+  },
+  setListId(state, listId) {
+    state.listId = listId
+  },
 }
 
 export const actions = {
   async fetchTeams({ commit }) {
     const { teams } = await this.$axios.$get('/api/v1/team')
-    commit(
-      'setTeams',
-      teams.map(e => {
-        return new Team(e.id, e.name)
-      }),
-    )
+    commit('setTeams', teams.map(e => new Team(e.id, e.name)))
   },
-  async fetchSpaces({ commit }, teamId) {
+  async fetchSpaces({ commit, state }) {
+    const { teamId } = state
     const { spaces } = await this.$axios.$get(`/api/v1/team/${teamId}/space`)
-    commit(
-      'setSpaces',
-      spaces.map(e => {
-        return new Space(e.id, e.name)
-      }),
-    )
+    commit('setSpaces', spaces.map(e => new Space(e.id, e.name)))
   },
-  async fetchProjects({ commit }, spaceId) {
+  async fetchProjects({ commit, state }) {
+    const { spaceId } = state
     const { projects } = await this.$axios.$get(
       `/api/v1/space/${spaceId}/project`,
     )
@@ -80,19 +139,19 @@ export const actions = {
         return new Project(
           e.id,
           e.name,
-          e.lists.map(e => {
-            return new List(e.id, e.name)
-          }),
+          e.lists.map(e => new List(e.id, e.name)),
         )
       }),
     )
   },
-  async fetchTasks({ commit }, { teamId, projectId, listId }) {
-    const params = { subtasks: true }
-    if (isFinite(projectId)) {
-      params.project_ids = [projectId]
+  async fetchTasks({ commit, state }) {
+    const { teamId, listId, projectId } = state
+    const params = {
+      subtasks: true,
+      project_ids: [projectId],
     }
-    if (isFinite(listId)) {
+
+    if (listId !== null) {
       params.list_ids = [listId]
     }
 
@@ -110,9 +169,7 @@ export const actions = {
           e.id,
           e.name,
           e.text_content,
-          e.assignees.map(e => {
-            return new Assigne(e.id, e.username)
-          }),
+          e.assignees.map(e => new Assigne(e.id, e.username)),
           e.start_date !== null ? moment(e.start_date) : moment(e.date_created),
           e.due_date !== null ? moment(e.due_date) : null,
         )
